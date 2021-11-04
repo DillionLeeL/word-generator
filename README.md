9331c7523e9486a35c3b7ffc7228180c9aa92a03
Dillion Lowry <dll3k@mtmail.mtsu.edu>
2021-11-04 11:06:54 -0500

    Added --fix argument, see readme

--- README.md
+++ README.md
@@ -5,8 +5,17 @@ Good for creative writing, placeholder names, brainstorming.
 
 Input: text file of sample words, newline delimited
 
-output: prints 10 generated words
+output: prints generated words
 
-Words are (currently) capped at 5 "parts" and the vowel it is split at goes to the "left" part
+Words are split into "parts", similar to syllables, and the vowel it is split at goes to the "left" part
+ex - if max part length is 5, then "conceptualization" becomes ["co", "nce", "ptu", "a", "li"]
 
-ex - "conceptualization" becomes ["co", "nce", "ptu", "a", "li"]
+Uses the argparse module for command line arguments. Usage:
+
+  -h, --help            show this help message and exit
+  --file FILE           File to import samples from
+  --c {1-100}           How many words to generate (1-100)
+  --l {1-10}            Max part length of generated words (1-10)
+  --no_order            Do not keep order of word parts, can make unintelligible results.
+  --order               Keep order of word parts, more closely matches samples.
+  --fix                 Attempt to break up long strings of consonants during generation. Will not work if samples contain them.
--- wordgenerator.py
+++ wordgenerator.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+import random
 import argparse
 
 def position_of_first_vowel(word):
@@ -42,7 +43,7 @@ def generate_parts(word_list, num_parts):
         max_parts=max(max_parts,seen_parts)
     return parts_list, min(num_parts, max_parts)
 
-def make_words(part_list, num_parts, num_words, order):
+def make_words(part_list, num_parts, num_words, order, fix_con):
     for x in range(num_words):
         word = ""
         # Get a number of parts between 2 and num_parts
@@ -54,8 +55,12 @@ def make_words(part_list, num_parts, num_words, order):
                     part = part_list[np.random.randint(len(part_list))][col]
                 else:
                     part = part_list[np.random.randint(len(part_list))][np.random.randint(num_parts)]
+
+            # Check that there won't be 3+ consonants
+            if fix_con and len(word)>=2 and word[-1] not in "aeiouy" and word[-2] not in "aeiouy":
+                word += random.choice("aeiou")
             word+=part
-        print(word)
+        print(word.strip())
 
 def main(args):
 
@@ -88,6 +93,10 @@ def main(args):
             keep_order= True
         else:
             keep_order= False
+    if args.fix:
+        add_vowels=True
+    else:
+        add_vowels=False
     
     try:
         with open(filename) as example_file:
@@ -95,19 +104,20 @@ def main(args):
 
         p_list, num_parts = generate_parts(example_list, num_parts)
 
-        make_words(p_list, num_parts, num_words, keep_order)
+        make_words(p_list, num_parts, num_words, keep_order, add_vowels)
 
         example_file.close()
-    except:
+    except BaseException as err:
         print("An error occured.")
+        print(err)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--file', type=str, help="File to import samples from")
     parser.add_argument('--c', type=int, choices=range(1,101), help="How many words to generate (1-100)")
-    parser.add_argument('--l', type=int, choices=range(1,11), help=('Enter the max part length of generated words (1-10)'))
-    parser.add_argument('--no_order', action='store_false', help="Do not keep order of word parts, can make unintelligible results")
-    parser.add_argument('--order', action='store_true', help="Keep order of word parts, more closely matches samples")
-
+    parser.add_argument('--l', type=int, choices=range(1,11), help=('Max part length of generated words (1-10)'))
+    parser.add_argument('--no_order', action='store_false', help="Do not keep order of word parts, can make unintelligible results.")
+    parser.add_argument('--order', action='store_true', help="Keep order of word parts, more closely matches samples.")
+    parser.add_argument('--fix', action='store_true', help="Attempt to break up long strings of consonants during generation. Will not work if samples contain them.")
     args = parser.parse_args()
     main(args)
